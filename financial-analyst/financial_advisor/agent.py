04ccf970124a6577d47eb0c731d3152d6bbd6ff8
kumrye <kumrye.park@gmail.com>
2026-04-27 23:00:39 +0530

    feat(financial-analyst): add initial implementation

---
+++ financial-analyst/financial_advisor/agent.py
@@ -0,0 +1,26 @@
+from google.adk.agents import Agent
+from google.adk.tools.agent_tool import AgentTool
+from google.adk.models.lite_llm import LiteLlm
+from .sub_agents.data_analyst import data_analyst
+from .sub_agents.financial_analyst import financial_analyst
+from .sub_agents.news_analyst import news_analyst
+from .prompt import PROMPT
+
+
+MODEL = LiteLlm("openai/gpt-4o")
+
+def save_advice_report():
+    pass
+
+financial_advisor = Agent(
+    name="FinancialAdvisor",
+    model=MODEL,
+    tools=[
+        AgentTool(agent=financial_analyst),
+        AgentTool(agent=news_analyst),
+        AgentTool(agent=data_analyst),
+        save_advice_report,
+    ],
+)
+
+root_agent = financial_advisor
